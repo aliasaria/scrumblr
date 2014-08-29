bafea024165a082f451e2ca9f573c4f9d39dff2b
Jez McKinley <jmckinley@peer1.com>
2014-08-29 23:38:01 +0100

    JM: made zIndex of cards dependent on position so you can stack them

--- client/script.js
+++ client/script.js
@@ -165,17 +165,20 @@ function drawNewCard(id, text, x, y, rot, style, sticker)
 	var ticketNumberRegex = new RegExp("^([A-Z]+-[0-9]+)", 'ig');
   var linksEnabled = text.replace(ticketNumberRegex,"<a href=http://jira.corp.peer1.net/browse/$1>$1</a>");
 
+	var zIndex = Math.round(x + (y*10));
+	if ('postit' == style) zIndex += 10000;
 	var cardFileName = style.replace(/^.* /, '') + '-card.png';
 
-	var h = '<div id="' + id + '" class="card ' + style + ' draggable clearfix" style="-webkit-transform:rotate(' + rot + 'deg);">\
-	<img src="/images/icons/token/Xion.png" class="card-icon delete-card-icon" />\
-	<img class="card-image" src="/images/' + cardFileName + '">\
-	<div id="content:' + id + '" class="content stickertarget droppable">' + linksEnabled + '</div>\
-	</div>';
+	var h = '<div id="' + id + '" ' +
+	'class="card ' + style + ' draggable clearfix" ' +
+	'style="-webkit-transform:rotate(' + rot + 'deg);z-index:' + zIndex + ';"> ' +
+	'<img src="/images/icons/token/Xion.png" class="card-icon delete-card-icon" />' +
+	'<img class="card-image" src="/images/' + cardFileName + '">' +
+	'<div id="content:' + id + '" class="content stickertarget droppable">' + linksEnabled + '</div>' +
+	'</div>';
 
 //	<div style="clear:both"></div>\
-
-        var card = $(h);
+  var card = $(h);
 	card.appendTo('#board');
 	
 	//@TODO
