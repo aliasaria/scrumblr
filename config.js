06fb9b155b341a681c44153309df206222a87a9c
Tim Toomey <tim@tim-mit.com>
2015-02-08 21:48:15 -0800

    Allow scrumblr to run on non-root paths successfully

--- config.js
+++ config.js
@@ -6,8 +6,25 @@
 };
 */
 
+var argv = require('yargs')
+        .usage('Usage: $0 [--port INTEGER [8080]] [--baseurl STRING ["/"]] [--redis STRING:INT [127.0.0.1:6379]] [--gaEnabled] [--gaAccount STRING [UA-2069672-4]]')
+        .argv;
+
+exports.server = {
+	port: argv.port || 8080,
+	baseurl: argv.baseurl || '/'
+};
+
+exports.googleanalytics = {
+	enabled: argv['gaEnabled'] || true,
+	account: argv['gaAccount'] || "UA-2069672-4"
+};
+
+var redis_conf = argv.redis || '127.0.0.1:6379';
 exports.database = {
 	type: 'redis',
-	prefix: '#scrumblr#'
+	prefix: '#scrumblr#',
+	host: redis_conf.split(':')[0] || '127.0.0.1',
+	port: redis_conf.split(':')[1] || 6379
 };
 
